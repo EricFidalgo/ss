88c06137dde7382ea579d2bca92ddf82f1bbe663
Eric Fidalgo <ericfidalgo.cs@gmail.com>
2025-11-02 18:45:21 -0500

    new MAUI folder structure

---
+++ Homework2.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Homework2.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Homework2.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Homework2.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
