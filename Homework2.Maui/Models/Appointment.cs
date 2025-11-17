f8dd334bcf87a1c9bfe3732299154e24c76ac7ae
Eric Fidalgo <ericfidalgo.cs@gmail.com>
2025-11-16 19:45:29 -0500

    fs

--- Homework2.Maui/Models/Appointment.cs
+++ Homework2.Maui/Models/Appointment.cs
@@ -1,10 +1,12 @@
 namespace Homework2.Maui.Models
 {
-public class Appointment
-{
-  public int Id { get; set; }
-  public Patient? patients;
-  public Physician? physicians;
-  public DateTime hour; 
-};
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        // CHANGE: Add { get; set; } to make these Properties instead of Fields
+        public Patient? patients { get; set; }
+        public Physician? physicians { get; set; }
+        public DateTime hour { get; set; } 
+    }
 }
